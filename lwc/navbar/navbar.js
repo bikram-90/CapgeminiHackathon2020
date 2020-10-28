3a094b1dc5dd1ab4789bc21fc1571d3a74a01e09
bikramDev <bikrambehera7@gmail.com>
2020-10-28 13:14:58 +0530

    Day2_28Oct2020_CustomTheme_CustomLayout

--- lwc/navbar/navbar.js
+++ lwc/navbar/navbar.js
@@ -3,19 +3,26 @@ import CNxFiles from '@salesforce/resourceUrl/CNxFiles';
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 import FontAwesome from '@salesforce/resourceUrl/FontAwesome';
 import GoogleFont from '@salesforce/resourceUrl/GoogleFont';
+//import propertyName from '@salesforce/community/property';
 
 export default class Navbar extends LightningElement {
+
+    get registerUrl() {
+        var urlString = window.location.href;
+        var baseURL = urlString.substring(0, urlString.indexOf("/s"));
+        return baseURL + '/s/customer-registration';
+    }
 
     get navBarLogoPath() {
         return CNxFiles + '/CNxFiles/Images/navbarlogo.svg';
     }
 
-    connectedCallback(){
+    connectedCallback() {
         console.log('INSIDE Navbar connectedCallback');
         loadScript(this, FontAwesome + '/fontawesome.js');
         loadStyle(this, GoogleFont + '/googleFont.css');
     }
-    renderedCallback(){
+    renderedCallback() {
         console.log('INSIDE Navbar renderedCallback');
         console.log("navBarLogoPath : " + this.navBarLogoPath);
     }
